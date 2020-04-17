c4be2affa4763af08a4a381e5dc1e7321890a8c5
Jorge Junior <jjorge98@outlook.com>
2020-04-17 00:19:37 -0300

    'fixed_all'

--- app/src/main/java/br/iesb/songs/views/PrincipalActivity.kt
+++ app/src/main/java/br/iesb/songs/views/PrincipalActivity.kt
@@ -2,7 +2,6 @@ package br.iesb.songs.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import br.iesb.songs.R
@@ -11,7 +10,6 @@ import kotlinx.android.synthetic.main.activity_principal.*
 
 class PrincipalActivity : AppCompatActivity() {
     private lateinit var mPagerAdapter: PagerAdapter
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,20 +23,15 @@ class PrincipalActivity : AppCompatActivity() {
         mViewPager.adapter = mPagerAdapter
         mViewPager.offscreenPageLimit = 3
 
-
         mViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
-
             }
-
             override fun onPageScrolled(
                 position: Int,
                 positionOffset: Float,
                 positionOffsetPixels: Int
             ) {
-
             }
-
             override fun onPageSelected(position: Int) {
                 changingTabs(position)
             }
@@ -57,15 +50,13 @@ class PrincipalActivity : AppCompatActivity() {
         }
         if (position == 1) {
             homeBtn.setImageResource(R.drawable.ic_home_menu)
-            pesquisaBtn.setImageResource(R.drawable.ic_search_menu_clarin)
-            favoritoBtn.setImageResource(R.drawable.ic_favorite_menu)
-
+            pesquisaBtn.setImageResource(R.drawable.ic_search_menu)
+            favoritoBtn.setImageResource(R.drawable.ic_favorite_menu_clarin)
         }
         if (position == 2) {
             homeBtn.setImageResource(R.drawable.ic_home_menu)
-            pesquisaBtn.setImageResource(R.drawable.ic_search_menu)
-            favoritoBtn.setImageResource(R.drawable.ic_favorite_menu_clarin)
-
+            pesquisaBtn.setImageResource(R.drawable.ic_search_menu_clarin)
+            favoritoBtn.setImageResource(R.drawable.ic_favorite_menu)
         }
     }
 }
